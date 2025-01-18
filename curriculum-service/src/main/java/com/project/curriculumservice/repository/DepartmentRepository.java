a333269440a0e0dfb475e9b134295919ea06f519
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-19 02:01:42 +0300

    Add Repo classes for - Department & Program

---
+++ curriculum-service/src/main/java/com/project/curriculumservice/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.project.curriculumservice.repository;
+
+import com.project.curriculumservice.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
