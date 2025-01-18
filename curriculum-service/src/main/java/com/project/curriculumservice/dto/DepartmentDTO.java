cf118ede1c5bb7c2811dca531f5f6872d943a9c3
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-19 01:59:59 +0300

    Add DTO classes for: - Department and Program

---
+++ curriculum-service/src/main/java/com/project/curriculumservice/dto/DepartmentDTO.java
@@ -0,0 +1,26 @@
+package com.project.curriculumservice.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DepartmentDTO {
+
+    private Long departmentId;
+
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    @NotEmpty(message = "Code is required")
+    private String code;
+
+    private String head;
+
+    @NotEmpty(message = "At least one program is required")
+    private Set<Long> programIds; // List of Program IDs to associate
+}
