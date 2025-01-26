af82727ea588862297faea7f1063a09eb0f87528
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 20:03:36 +0300

    Add getEnrollmentsByStudent method

--- curriculum-service/src/main/java/com/project/curriculumservice/feignclient/client/EnrollmentServiceClient.java
+++ curriculum-service/src/main/java/com/project/curriculumservice/feignclient/client/EnrollmentServiceClient.java
@@ -1,13 +1,22 @@
 package com.project.curriculumservice.feignclient.client;
 
+import com.project.curriculumservice.feignclient.dtos.EnrollmentResponseDTO;
 import com.project.curriculumservice.feignclient.fallback.EnrollmentServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @FeignClient(name = "enrollment-service", fallback = EnrollmentServiceFallback.class)
 public interface EnrollmentServiceClient {
+    @GetMapping("/api/enrollments/students/{studentId}")
+    ResponseEntity<List<EnrollmentResponseDTO>> getEnrollmentsByStudent(
+            @PathVariable int studentId,
+            @RequestParam(required = false) List<String> status);
 
     @GetMapping("/demo/hi")
     String hi();
+
 }
--- curriculum-service/src/main/java/com/project/curriculumservice/feignclient/fallback/EnrollmentServiceFallback.java
+++ curriculum-service/src/main/java/com/project/curriculumservice/feignclient/fallback/EnrollmentServiceFallback.java
@@ -1,8 +1,19 @@
 package com.project.curriculumservice.feignclient.fallback;
 
+import com.project.curriculumservice.dto.CourseOfferingResponseDTO;
 import com.project.curriculumservice.feignclient.client.EnrollmentServiceClient;
+import com.project.curriculumservice.feignclient.dtos.EnrollmentResponseDTO;
+import com.project.curriculumservice.feignclient.dtos.EnrollmentStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public class EnrollmentServiceFallback implements EnrollmentServiceClient {
+    @Override
+    public ResponseEntity<List<EnrollmentResponseDTO>> getEnrollmentsByStudent(int studentId, List<String> status) {
+        return null;
+    }
 
     @Override
     public String hi() {
