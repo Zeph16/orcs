144e23853cd3608e6a1729761a7b9111480afdc1
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 19:58:40 +0300

    Update fields with necessary validations

--- curriculum-service/src/main/java/com/project/curriculumservice/dto/CourseRequestDTO.java
+++ curriculum-service/src/main/java/com/project/curriculumservice/dto/CourseRequestDTO.java
@@ -2,6 +2,8 @@ package com.project.curriculumservice.dto;
 
 import com.project.curriculumservice.model.Course.CourseStatus;
 import com.project.curriculumservice.model.Course.CourseType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,11 +12,22 @@ import java.util.Set;
 @Data
 @Builder
 public class CourseRequestDTO {
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Code is required")
     private String code;
+
+    @NotNull(message = "Credit hours are required")
     private Integer creditHrs;
+
+    @NotNull(message = "Course type is required")
     private CourseType type;
+
+    @NotNull(message = "Course status is required")
     private CourseStatus status;
+
+    @NotNull(message = "Department program IDs are required")
     private Set<DepartmentProgramIdDTO> departmentProgramIds;
     private Set<Long> prerequisiteIds;
 }
