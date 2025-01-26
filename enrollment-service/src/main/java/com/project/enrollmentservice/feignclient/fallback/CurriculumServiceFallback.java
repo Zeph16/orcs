4db6d3bc19560a263508c91dced5e0b6332b9ba4
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 20:01:22 +0300

    Add getCourseOfferingById method

--- enrollment-service/src/main/java/com/project/enrollmentservice/feignclient/fallback/CurriculumServiceFallback.java
+++ enrollment-service/src/main/java/com/project/enrollmentservice/feignclient/fallback/CurriculumServiceFallback.java
@@ -1,11 +1,20 @@
 package com.project.enrollmentservice.feignclient.fallback;
 
 import com.project.enrollmentservice.feignclient.client.CurriculumServiceClient;
+import com.project.enrollmentservice.feignclient.dto.CourseOfferingResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public class CurriculumServiceFallback implements CurriculumServiceClient {
 
     @Override
     public String hi() {
         return "Oops, can't reach curriculum-service /demo/hi endpoint!";
+    }
+    @Override
+    public ResponseEntity<CourseOfferingResponseDTO> getCourseOfferingById(Long offeringId) {
+        CourseOfferingResponseDTO fallbackResponse = new CourseOfferingResponseDTO();
+        fallbackResponse.setInstructorName("ERROR: THIS IS FALLBACK RESPONSE");
+        return ResponseEntity.ok(fallbackResponse);
     }
 }
