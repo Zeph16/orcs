5af86b5fb877c45c0c1b5522dcf48d391832f610
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 20:03:01 +0300

    Update fields with necessary validations

--- curriculum-service/src/main/java/com/project/curriculumservice/dto/DepartmentProgramIdDTO.java
+++ curriculum-service/src/main/java/com/project/curriculumservice/dto/DepartmentProgramIdDTO.java
@@ -1,12 +1,16 @@
 package com.project.curriculumservice.dto;
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class DepartmentProgramIdDTO {
+    @NotNull(message = "Department ID is required")
     private Long departmentId;
+
+    @NotNull(message = "Program ID is required")
     private Long programId;
 }
