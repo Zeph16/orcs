53184a55bcbc9944042711ff55e345e7578bc98b
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 19:53:34 +0300

    Remove unnecessary termID field and required constraint on the section field

--- student-service/src/main/java/com/project/studentservice/dto/BatchRequestDTO.java
+++ student-service/src/main/java/com/project/studentservice/dto/BatchRequestDTO.java
@@ -1,20 +1,13 @@
 package com.project.studentservice.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 
 @Data
@@ -30,10 +23,6 @@ public class BatchRequestDTO {
 
     private String code;
 
-    @NotNull(message = "Term ID is required")
-    private Integer termId;
-
-    @NotNull(message = "Section is required")
     private Character section;
 
     @NotNull(message = "Expected Graduation Date is required")
