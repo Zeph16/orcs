49b45fa6a8a70b784a16ec4f42dd1906368fca72
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 20:01:07 +0300

    Add getCourseOfferingById method

--- enrollment-service/src/main/java/com/project/enrollmentservice/feignclient/client/CurriculumServiceClient.java
+++ enrollment-service/src/main/java/com/project/enrollmentservice/feignclient/client/CurriculumServiceClient.java
@@ -1,12 +1,18 @@
 package com.project.enrollmentservice.feignclient.client;
 
+import com.project.enrollmentservice.feignclient.dto.CourseOfferingResponseDTO;
 import com.project.enrollmentservice.feignclient.fallback.CurriculumServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "curriculum-service", fallback = CurriculumServiceFallback.class)
 public interface CurriculumServiceClient {
-
     @GetMapping("/demo/hi")
     String hi();
+    @GetMapping("/api/course-offerings/{offeringId}")
+    ResponseEntity<CourseOfferingResponseDTO> getCourseOfferingById(
+            @PathVariable Long offeringId);
+
 }
