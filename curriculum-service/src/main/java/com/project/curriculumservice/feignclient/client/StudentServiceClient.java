4c53caab3de492b26ee2d838afffd775ddd42995
Abel Gebeyehu <abelgebeyehu101@gmail.com>
2025-01-26 20:08:23 +0300

    Add getBatchById & getStudentById methods

--- curriculum-service/src/main/java/com/project/curriculumservice/feignclient/client/StudentServiceClient.java
+++ curriculum-service/src/main/java/com/project/curriculumservice/feignclient/client/StudentServiceClient.java
@@ -1,12 +1,20 @@
 package com.project.curriculumservice.feignclient.client;
 
+import com.project.curriculumservice.feignclient.dtos.BatchResponseDTO;
+import com.project.curriculumservice.feignclient.dtos.StudentResponseDTO;
 import com.project.curriculumservice.feignclient.fallback.StudentServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "student-service", fallback = StudentServiceFallback.class)
 public interface StudentServiceClient {
 
     @GetMapping("/demo/hi")
     String hi();
+    @GetMapping("/api/batches/id/{id}")
+    ResponseEntity<BatchResponseDTO> getBatchById(@PathVariable Long id);
+    @GetMapping("/api/students/{id}")
+    public ResponseEntity<StudentResponseDTO> getStudentById(@PathVariable int id);
 }
--- curriculum-service/src/main/java/com/project/curriculumservice/feignclient/fallback/StudentServiceFallback.java
+++ curriculum-service/src/main/java/com/project/curriculumservice/feignclient/fallback/StudentServiceFallback.java
@@ -1,11 +1,31 @@
 package com.project.curriculumservice.feignclient.fallback;
 
+import com.project.curriculumservice.feignclient.dtos.BatchResponseDTO;
 import com.project.curriculumservice.feignclient.client.StudentServiceClient;
+import com.project.curriculumservice.feignclient.dtos.StudentResponseDTO;
+import org.springframework.http.ResponseEntity;
 
 public class StudentServiceFallback implements StudentServiceClient {
 
     @Override
     public String hi() {
         return "Oops, can't reach student-service /demo/hi endpoint!";
+    }
+    @Override
+    public ResponseEntity<BatchResponseDTO> getBatchById(Long id) {
+        // Provide a default or fallback response
+        BatchResponseDTO fallbackResponse = new BatchResponseDTO();
+        fallbackResponse.setId(id);
+        fallbackResponse.setCode("Code Unavailable");
+        return ResponseEntity.ok(fallbackResponse);
+    }
+
+    @Override
+    public ResponseEntity<StudentResponseDTO> getStudentById(int id) {
+        // Provide a default or fallback response
+        StudentResponseDTO fallbackResponse = new StudentResponseDTO();
+        fallbackResponse.setId(id);
+        fallbackResponse.setName("ERROR: THIS IS FALLBACK RESPONSE");
+        return ResponseEntity.ok(fallbackResponse);
     }
 }
