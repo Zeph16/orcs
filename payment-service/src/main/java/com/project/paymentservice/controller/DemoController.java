78bd8f0a142b85672856ff5210949b8e90c750fd
Sofonias Ayele <sof16ayele@gmail.com>
2025-02-14 14:08:24 +0300

    Implemented security in the payment service

--- payment-service/src/main/java/com/project/paymentservice/controller/DemoController.java
+++ payment-service/src/main/java/com/project/paymentservice/controller/DemoController.java
@@ -5,9 +5,17 @@ import com.project.paymentservice.feignclient.client.SchedulingServiceClient;
 import com.project.paymentservice.feignclient.client.EnrollmentServiceClient;
 import com.project.paymentservice.feignclient.client.StudentServiceClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/demo")
@@ -21,6 +29,27 @@ public class DemoController {
     @GetMapping("/hi")
     public String hi() {
         return "Hi from payment-service!";
+    }
+
+    @PreAuthorize("hasRole('student')")
+    @GetMapping("/auth")
+    public String auth(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+
+        String username = null;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String roles = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(", "));
+
+        // Return the username and roles as a string
+        return "User: " + username + ", Roles: " + roles;
     }
 
     @GetMapping("/hi-student")
---
+++ payment-service/src/main/java/com/project/paymentservice/util/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.project.paymentservice.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(
+                                "/api/payments/**"
+                        ).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                        )
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
+                );
+
+        return http.build();
+    }
+}
